50a21088a4d247ca951f0761550c9eada133ad90
savysanish56 <86150580+savysanish56@users.noreply.github.com>
2022-09-12 21:31:22 +0930

    proc and requiremnts

--- Astra/settings.py
+++ Astra/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@l%cz_h5ph#7w^(1+u8hg9*t4ajwqpt=g!%rh615wszvx(a-rh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
