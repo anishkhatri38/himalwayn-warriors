b135c359a852a038729c8ff2f7e511f76b259927
savysanish56 <86150580+savysanish56@users.noreply.github.com>
2022-09-19 00:53:11 +0930

    testing

--- Astra/settings.py
+++ Astra/settings.py
@@ -219,7 +219,7 @@ STRIPE_WEBHOOK_SECRET = ""
 
 
 #Image configuration in S3 Bucket 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 AWS_QUERYSTRING_AUTH = False
 AWS_S3_FILE_OVERWRITE = False 
@@ -228,3 +228,4 @@ AWS_ACCESS_KEY_ID = 'AKIA2PYQACGPI3XKK2H2'
 AWS_SECRET_ACCESS_KEY = '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'
 AWS_STORAGE_BUCKET_NAME = 'anishkhatri'
 AWS_S3_SIGNATURE_VERSION = "s3v4"
+
