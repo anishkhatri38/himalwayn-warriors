d46510295076ae3b68f86579c60cfcbf3c948f42
savysanish56 <86150580+savysanish56@users.noreply.github.com>
2022-10-04 02:39:26 +1030

    Shipping and product cart update

--- Products/views.py
+++ Products/views.py
@@ -1,4 +1,3 @@
-import json
 from django.shortcuts import render
 from django.http import JsonResponse
 import json
@@ -10,8 +9,18 @@ from .models import *
 
 
 def products(request):
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer = customer, complete = False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
+        cartItems = order.get_cart_items
     products = Product.objects.all()
-    context = {'products':products}
+    context = {'products':products,'cartItems':cartItems}
     return render(request,'Products/products_home.html',context )
 
 
@@ -26,12 +35,12 @@ def product_checkout(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer = customer, complete = False)
         items = order.orderitem_set.all()
-        cartItems = order['get_cart_items']
+        cartItems = order.get_cart_items
     else:
         items = []
         order = {'get_cart_total':0, 'get_cart_items':0}
-        cartItems = order.get_cart_items
-    context = {'items':items,'order':order,}
+        cartItems = order['get_cart_items']
+    context = {'items':items,'order':order,'cartItems':cartItems,'shipping':False}
     return render(request,'Products/product_checkout.html',context)
 
 
@@ -43,11 +52,33 @@ def product_cart(request):
     else:
         items = []
         order = {'get_cart_total':0, 'get_cart_items':0}
+        cartItems = order['get_cart_items']
 
-    context = {'items':items,'order':order}
+    context = {'items':items,'order':order,'shipping':False}
     return render(request,'Products/product_cart.html',context)
 
 
 def updateItem(request):
-    return JsonResponse('Item was added', safe = False)
-    
+	data = json.loads(request.body)
+	productId = data['productId']
+	action = data['action']
+	print('Action:', action)
+	print('Product:', productId)
+
+	customer = request.user.customer
+	product = Product.objects.get(id=productId)
+	order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+	orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+	if action == 'add':
+		orderItem.quantity = (orderItem.quantity + 1)
+	elif action == 'remove':
+		orderItem.quantity = (orderItem.quantity - 1)
+
+	orderItem.save()
+
+	if orderItem.quantity <= 0:
+		orderItem.delete()
+
+	return JsonResponse('Item was added', safe=False)
