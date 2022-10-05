ed2c114469fc536b77e9d12cc460b181163fd27a
savysanish56 <86150580+savysanish56@users.noreply.github.com>
2022-10-05 17:17:43 +1030

    a

--- Astra/settings.py
+++ Astra/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'storages'
+    'storages',
 ]
 JAZZMIN_UI_TWEAKS = {
     "theme": "slate",
