069d10dc30d63b49b68a4213acbf135873a4f7e4
anishkhatri38 <93702336+anishkhatri38@users.noreply.github.com>
2022-12-24 18:05:19 +1030

    Update settings.py

--- Astra/settings.py
+++ Astra/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-@l%cz_h5ph#7w^(1+u8hg9*t4ajwqpt=g!%rh615wszvx(a-rh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://web-production-e8a2.up.railway.app']
 
 
 # Application definition
